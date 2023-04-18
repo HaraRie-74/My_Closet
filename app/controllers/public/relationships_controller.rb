0fad44d57af902eddfe0db15fe36d2b83a1a3b46
HaraRie-74 <121597855+HaraRie-74@users.noreply.github.com>
2023-04-18 02:18:10 +0000

    [add]フォロー機能非同期通信化

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,13 +1,11 @@
 class Public::RelationshipsController < ApplicationController
   def create
-    other_user = User.find(params[:user_id])
-    current_user.follow(other_user)
-    redirect_to request.referer
+    @other_user = User.find(params[:user_id])
+    current_user.follow(@other_user)
   end
 
   def destroy
-    other_user = User.find(params[:user_id])
-    current_user.unfollow(other_user)
-    redirect_to request.referer
+    @other_user = User.find(params[:user_id])
+    current_user.unfollow(@other_user)
   end
 end

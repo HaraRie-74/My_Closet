201ad265e6dd244dcdedeaa306a911d6a337f3f2
HaraRie-74 <121597855+HaraRie-74@users.noreply.github.com>
2023-04-18 04:39:13 +0000

    [add]コメント非同期通信化

--- app/controllers/public/closet_comments_controller.rb
+++ app/controllers/public/closet_comments_controller.rb
@@ -3,16 +3,17 @@ class Public::ClosetCommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    closet = Closet.find(params[:closet_id])
+    @closet = Closet.find(params[:closet_id])
     comment = current_user.closet_comments.new(closet_comment_params)
-    comment.closet_id = closet.id
+    comment.closet_id = @closet.id
     comment.save
-    redirect_to closet_path(closet.id)
+    @comment = ClosetComment.new
   end
 
   def destroy
+    @closet = Closet.find(params[:closet_id])
     ClosetComment.find(params[:id]).destroy
-    redirect_to closet_path(params[:closet_id])
+    @comment = ClosetComment.new
   end
 
 
